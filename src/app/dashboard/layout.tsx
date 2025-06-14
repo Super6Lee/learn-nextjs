563e1beabb19877dedbd0d502bf411b28274edb1
Super6Lee <a1530633034@163.com>
2025-06-14 14:21:08 +0800

    usePathname 学习

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { useState } from "react";
 
 export default function DashboardLayout({
@@ -10,13 +11,16 @@ export default function DashboardLayout({
 }>) {
 
   const [count, setCount] = useState(0);
+  const pathname = usePathname();
 
   return (
+
+
     <div className="border-2 border-dashed border-black p-4 w-1/2 mx-auto">
       
       <div className="flex gap-4 font-bold text-lg mb-4 text-purple-500">
-        <Link href="/dashboard/about">About</Link>
-        <Link href="/dashboard/settings">Settings</Link>
+        <Link className={pathname === "/dashboard/about" ? "text-red-500 active" : ""} href="/dashboard/about">About</Link>
+        <Link className={pathname === "/dashboard/settings" ? "text-red-500 active" : ""} href="/dashboard/settings">Settings</Link>
       </div>
       <h2> Dashboard layout {count}</h2>
       <button className="bg-black text-white p-2 rounded-md" onClick={() => setCount(count + 1)}>Increment</button>
