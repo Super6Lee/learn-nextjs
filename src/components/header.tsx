73b6fb38e144d60c13b26697b3ea108e2bc777f2
Super6Lee <a1530633034@163.com>
2025-06-15 00:23:53 +0800

    404 页面 去掉导航栏

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,35 +4,40 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import React from "react";
 
+const linkData = [
+  {name: "Perfomance", href: "/perfomance"},
+  {name: "Reliability", href: "/reliability"},
+  {name: "Scale", href: "/scale"},
+];
+
+const accessLink = linkData.map(link => link.href);
+accessLink.push("/");
+
 export default function Header() {
   const pathname = usePathname();
 
+  if (!accessLink.includes(pathname)) {
+    return null;
+  }
+
   return (
     <nav className="absolute w-full z-10">
-      <div className="flex justify-between container mx-auto text-white p-8 items-center">
+      <div className="flex justify-between container mx-auto text-white p-8 links-center">
         <Link className="text-3xl font-bold" href="/">
           Home
         </Link>
 
         <div className="text-xl space-x-5">
-          <Link
-            className={pathname === "/perfomance" ? "text-red-500 active" : ""}
-            href="/perfomance"
-          >
-            Perfomance
-          </Link>
-          <Link
-            className={pathname === "/reliability" ? "text-red-500 active" : ""}
-            href="/reliability"
-          >
-            Reliability
-          </Link>
-          <Link
-            className={pathname === "/scale" ? "text-red-500 active" : ""}
-            href="/scale"
-          >
-            Scale
-          </Link>
+
+          {linkData.map((link) => (
+            <Link
+              key={link.href}
+              className={pathname === link.href ? "text-red-500 active" : ""}
+              href={link.href}
+            >
+              {link.name}
+            </Link>
+          ))}
         </div>
       </div>
     </nav>
