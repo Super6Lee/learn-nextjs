67811abbef25a0bc3b050426c73458b5a02105b8
Super6Lee <a1530633034@163.com>
2025-06-15 23:26:38 +0800

    平行路由案例

---
+++ src/app/@analytics/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div className="flex justify-center items-center h-60 flex-1 rounded-lg bg-purple-500 text-white">
+      @analytics
+    </div>
+  )
+}
--- src/app/default.tsx
+++ src/app/default.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Home() {
   return (
     <div className="flex mt-6 p-6 h-32 bg-gray-500 rounded-lg text-white">
-      Home
+      Home default
     </div>
   );
 };
