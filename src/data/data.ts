8ae8cc782c9735220fa6d7bbc306b37a517997a5
Super6Lee <a1530633034@163.com>
2025-06-15 23:41:29 +0800

    init

---
+++ src/data/data.ts
@@ -0,0 +1,50 @@
+export const photos = [
+    {
+      id: "1",
+      src: "https://img2.baidu.com/it/u=3107603990,1478027452&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=500",
+      alt: "Earthen Bottle",
+      price: 4,
+    },
+    {
+      id: "2",
+      src: "https://images-cn.ssl-images-amazon.cn/images/I/81StXa+kV5L._AC_SX679_.jpg",
+      alt: "Nomad Tumbler",
+      price: 7,
+    },
+    {
+      id: "3",
+      src: "https://img1.baidu.com/it/u=892819517,710532383&fm=253&fmt=auto&app=138&f=JPEG?w=667&h=500",
+      alt: "Focus Paper Refill",
+      price: 35,
+    },
+    {
+      id: "4",
+      src: "https://img0.baidu.com/it/u=3108854336,2202601455&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=500",
+      alt: "Machined Mechanical Pencil",
+      price: 16,
+    },
+    {
+      id: "5",
+      src: "https://test.zhihur.com/img/5.png",
+      alt: "Leslie Alexander",
+      price: 19,
+    },
+    {
+      id: "6",
+      src: "https://test.zhihur.com/img/6.png",
+      alt: "Michael Foster",
+      price: 69,
+    },
+    {
+      id: "7",
+      src: "https://test.zhihur.com/img/7.png",
+      alt: "Dries Vincent",
+      price: 22,
+    },
+    {
+      id: "8",
+      src: "https://test.zhihur.com/img/8.png",
+      alt: "Lindsay Walton",
+      price: 87,
+    },
+  ];
