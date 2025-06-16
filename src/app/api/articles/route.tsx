c1345eecf00afdc48ca84419df17b81489cd6376
Super6Lee <a1530633034@163.com>
2025-06-17 00:15:28 +0800

    分页查询

--- src/app/api/articles/route.tsx
+++ src/app/api/articles/route.tsx
@@ -1,9 +1,33 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import db from "@/db";
 
 // GET /api/articles
-export async function GET(request: Request) {
-  // ...
+export async function GET(request: NextRequest) {
+  
+  const searchParams = request.nextUrl.searchParams;
+
+  const pageNum = Number(searchParams.get("pageNum")) || 1;
+  const pageSize = Number(searchParams.get("pageSize")) || 2;
+  const title = searchParams.get("title") || "";
+  const content = searchParams.get("content") || "";
+
+  const data = db.data.posts;
+
+  const filteredData = data.filter((item) => {
+    return (
+      item.title.includes(title.toLowerCase()) &&
+      item.content.includes(content.toLowerCase())
+    );
+  });
+
+  const total = filteredData.length;
+
+  const startIndex = (pageNum - 1) * pageSize;
+  const endIndex = Math.min(startIndex + pageSize, total);
+  const paginatedData =
+    startIndex < endIndex ? filteredData.slice(startIndex, endIndex) : [];
+
+  return NextResponse.json({ data: paginatedData, total, pageNum, pageSize });
 }
 
 // POST /api/articles
